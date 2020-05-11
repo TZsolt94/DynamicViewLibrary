6cc7e5c848c3e9d057c58489a1d1d22bc37681dc
TZsolt94 <blackcrazy00@gmail.com>
2020-05-11 21:39:10 +0200

    Tesztelés, javítás
    Két oszlopos példa
    Javítások

--- app/src/main/java/com/example/dynamicviewexample/MainActivity.kt
+++ app/src/main/java/com/example/dynamicviewexample/MainActivity.kt
@@ -10,13 +10,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var dynVLH : DynamicViewListHolder
+    lateinit var dynVLH1 : DynamicViewListHolder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         dynVLH = DynamicViewListHolder(this, 3, arrayOf(StartButton, ProfileButton, OptionsButton, StatisticsButton, BMButton, ShopButton, puMButton))
+        dynVLH1 = DynamicViewListHolder(this, 3, arrayOf(StartButton1, ProfileButton1, OptionsButton1, StatisticsButton1, BMButton1, ShopButton1, puMButton1))
 
+        val clickListenerForDynViews1 = View.OnClickListener { view ->
+            dynVLH1.onClick(view as Button)
+
+            when (view.getId()) {
+                R.id.StartButton1 -> print(1)
+                R.id.OptionsButton1 -> print(2)
+            }
+        }
         val clickListenerForDynViews = View.OnClickListener { view ->
             dynVLH.onClick(view as Button)
 
@@ -27,14 +37,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         StartButton.setOnClickListener(clickListenerForDynViews)
+        StartButton1.setOnClickListener(clickListenerForDynViews1)
         OptionsButton.setOnClickListener(clickListenerForDynViews)
+        OptionsButton1.setOnClickListener(clickListenerForDynViews1)
         ProfileButton.setOnClickListener(clickListenerForDynViews)
+        ProfileButton1.setOnClickListener(clickListenerForDynViews1)
         BMButton.setOnClickListener(clickListenerForDynViews)
+        BMButton1.setOnClickListener(clickListenerForDynViews1)
         ShopButton.setOnClickListener(clickListenerForDynViews)
+        ShopButton1.setOnClickListener(clickListenerForDynViews1)
         StatisticsButton.setOnClickListener(clickListenerForDynViews)
+        StatisticsButton1.setOnClickListener(clickListenerForDynViews1)
 
         puMButton.setOnClickListener {
             dynVLH.showPopup(puMButton)
+        }
+        puMButton1.setOnClickListener {
+            dynVLH1.showPopup(puMButton1)
         }
         ExitButton.setOnClickListener {
             this.finish()
@@ -42,12 +61,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onResume() {
-        dynVLH.createDynamicView(R.id.constraintLayout, true)
+        dynVLH1.createConstraints(R.id.constraintLayout1, false)
+        dynVLH.createConstraints(R.id.constraintLayout2, false)
         super.onResume()
     }
 
     override fun onStop() {
         dynVLH.saveFrequency()
+        dynVLH1.saveFrequency()
         super.onStop()
     }
 }
--- dynamicview/src/main/java/com/example/dynamicview/DynamicView.kt
+++ dynamicview/src/main/java/com/example/dynamicview/DynamicView.kt
@@ -7,6 +7,7 @@ import android.util.DisplayMetrics
 import android.view.View
 import android.widget.Button
 import android.widget.PopupMenu
+import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 
@@ -14,20 +15,24 @@ class DynamicView(val button: Button, var usageNumber: Int): Comparable<DynamicV
     override fun compareTo(other: DynamicView): Int {
         return this.usageNumber.compareTo(other.usageNumber)
     }
+
+    fun onClick(){
+        usageNumber++
+    }
 }
 
-class DynamicViewListHolder(private val act: Activity, var visibleButtonNumber : Int, bArray : Array<Button>){
-    private var dvList = this.createDynViews(act, bArray)
+class DynamicViewListHolder(private val act: Activity, private val visibleButtonNumber : Int, bArray : Array<Button>){
+    private var dvList = this.createDynViewsList(bArray)
 
     fun onClick(button: Button) {
         for (item in dvList) {
             if (button.id == item.button.id) {
-                item.usageNumber++
+                item.onClick()
             }
         }
     }
 
-    fun hideRecentlyUsedButtons(){
+    private fun hideRecentlyUsedButtons(){
         for (i in 0 until dvList.size-1){
             if (i >= visibleButtonNumber) {
                 act.findViewById<Button>(dvList[i].button.id).visibility = View.GONE
@@ -38,7 +43,7 @@ class DynamicViewListHolder(private val act: Activity, var visibleButtonNumber :
         }
     }
 
-    private fun createDynViews(act : Activity, bArray : Array<Button>) : List<DynamicView>{
+    private fun createDynViewsList(bArray : Array<Button>) : List<DynamicView>{
         val sharedPref = act.getPreferences(Context.MODE_PRIVATE)
         val dvList : MutableList<DynamicView> = mutableListOf()
 
@@ -49,7 +54,7 @@ class DynamicViewListHolder(private val act: Activity, var visibleButtonNumber :
         return dvList
     }
 
-    fun createDynamicView(constraintLayoutID: Int, dynamicButtonReSize : Boolean, marginTop : Int = 0, marginButton : Int = 0, topViewID : Int = constraintLayoutID) {
+    fun createConstraints(constraintLayoutID: Int, dynamicButtonReSize : Boolean, marginTop : Int = 0, marginButton : Int = 0, topViewID : Int = constraintLayoutID) {
         val constraintLayout = act.findViewById<ConstraintLayout>(constraintLayoutID)
         var topView : View = constraintLayout
         if(topViewID != constraintLayoutID){
@@ -73,7 +78,7 @@ class DynamicViewListHolder(private val act: Activity, var visibleButtonNumber :
         var quotient:Double = 1.0
         for (i in 0 until dvList.size-1){
             if(dynamicButtonReSize) {
-                dvList[i].button.width = (width / (i + quotient)).toInt() + width / 4
+                dvList[i].button.width = (width*quotient).toInt()
             }
             constraintSet.connect(
                 dvList[i+1].button.id,
@@ -82,7 +87,7 @@ class DynamicViewListHolder(private val act: Activity, var visibleButtonNumber :
                 ConstraintSet.BOTTOM,
                 marginButton
             )
-            quotient+=0.1
+            quotient-=0.1
         }
 
         constraintSet.applyTo(constraintLayout)
